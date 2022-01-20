986e255906661ab35c85b4328cc3b060d32db43d
Ron <dicer1@ferris.edu>
2022-01-20 16:46:56 -0500

    Week 2 - Orbit

---
+++ Sketches/WaveDemo/WaveDemo.pde
@@ -0,0 +1,23 @@
+
+
+
+void setup() {
+  size(800, 500);
+  stroke(150, 0, 255);
+}
+
+void draw() {
+  background(64);
+
+  float time = millis()/1000.0;
+
+  // -1 to 1 (2)
+  // 2 to 20 (18)
+  float t = sin(time * 2) * 9 + 11; //-9 to 9
+
+
+
+  strokeWeight(t);
+
+  ellipse(width/2, height/2, 400, 400);
+}
--- UnityProject/Assets/Scripts/OrbitDemo.cs
+++ UnityProject/Assets/Scripts/OrbitDemo.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimMath : MonoBehaviour
+public class OrbitDemo : MonoBehaviour
 {
+    public Transform 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,12 @@ public class AnimMath : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if(!orbitCenter)
+        float x = Mathf.Cos(Time.time);
+        float z = Mathf.Sin(Time.time);
+
+        transform.position = new Vector3(x, 0, z) + orbitCenter.position;
+
     }
 }
